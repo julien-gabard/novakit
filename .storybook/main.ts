a984af8f5f72047095d00c3528ddc8417b2ea553
JulienGabard <julien.gabard@octaave.com>
2024-04-18 00:17:22 +0200

    Add file Introduction.mdx and licence.mdx and edit README.md for .mdx

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,12 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
-    stories: ['../src/**/__docs__/*.stories.tsx', '../src/**/__docs__/*.mdx'],
+    stories: [
+        './Introduction.mdx',
+        './Licence.mdx',
+        '../src/**/__docs__/*.stories.tsx',
+        '../src/**/__docs__/*.mdx',
+    ],
     addons: [
         '@storybook/addon-links',
         '@storybook/addon-essentials',
