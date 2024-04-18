a97bbfa48d2e90595af0c7eef233688e1a2e6338
JulienGabard <julien.gabard@octaave.com>
2024-04-18 17:11:02 +0200

    Add novakitTheme for manager storybook

---
+++ .storybook/novakitTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create'
+
+export default create({
+    base: 'dark',
+    brandTitle: 'Novakit',
+    brandUrl: 'https://novakit.com',
+    brandImage: '',
+    brandTarget: '_self',
+})
