292d305ff6b439a5e8aa190e701483a16f089d6d
Julien Gabard <57756925+julien-gabard@users.noreply.github.com>
2024-05-09 12:48:39 +0200

    Add @storybook/addon-a11y for test accessibility (#9)
    * Add @storybook/addon-a11y for test accessibility
    
    * Change brandUrl un novakitTheme.js
    
    ---------
    
    Co-authored-by: JulienGabard <julien.gabard@octaave.com>

--- .storybook/novakitTheme.js
+++ .storybook/novakitTheme.js
@@ -3,7 +3,7 @@ import { create } from '@storybook/theming/create'
 export default create({
     base: 'dark',
     brandTitle: 'Novakit',
-    brandUrl: 'https://novakit.com',
+    brandUrl: 'https://novakit.netlify.app',
     brandImage: '',
     brandTarget: '_self',
 })
